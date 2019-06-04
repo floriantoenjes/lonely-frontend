96d12e9ebda40b29312b27ebaddc3969ceac2513
Florian Tönjes <skatepainter@yahoo.de>
2019-06-04 18:39:09 +0200

    Created AuthInterceptor

---
+++ src/app/shared/interceptors/auth.interceptor.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { AuthService } from '../services/auth.service';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  private noAuthRoutes = [
+    '/sign-in'
+  ];
+
+  constructor(private authService: AuthService) {
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const route = req.url.replace(environment.basePath, '');
+
+    if (!this.noAuthRoutes.includes(route)) {
+
+      const authReq = req.clone({
+        headers: req.headers.set('Authorization', this.authService.getToken())
+      });
+
+      return next.handle(authReq);
+    } else {
+      return next.handle(req);
+    }
+  }
+}
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -33,4 +33,8 @@ export class AuthService {
   setToken(token: string): void {
     localStorage.setItem('token', token);
   }
+
+  getToken(): string {
+    return localStorage.getItem('token');
+  }
 }
