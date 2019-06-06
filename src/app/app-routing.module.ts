c494a70833a57ec1f2e531d0c3c73341f7bea81f
Florian Tönjes <skatepainter@yahoo.de>
2019-06-06 19:46:06 +0200

    Added SignedOutGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { SignInComponent } from './sign-in/sign-in.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SignedInGuard } from './shared/guards/signed-in.guard';
+import { SignedOutGuard } from './shared/guards/signed-out.guard';
 
 const routes: Routes = [
-  { path: 'sign-in', component: SignInComponent },
-  { path: 'profile', component: ProfileComponent, canActivate: [SignedInGuard]}
+  { path: 'sign-in', component: SignInComponent, canActivate: [SignedOutGuard] },
+  { path: 'profile', component: ProfileComponent, canActivate: [SignedInGuard] },
+  { path: '', redirectTo: 'profile', pathMatch: 'full' }
 ];
 
 @NgModule({
