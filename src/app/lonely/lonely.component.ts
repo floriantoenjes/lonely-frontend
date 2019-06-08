c06d9abd53b3b31b2539a74bb174a2b63ae6c007
Florian Tönjes <skatepainter@yahoo.de>
2019-06-08 18:03:59 +0200

    Implemented isLonely check

--- src/app/lonely/lonely.component.ts
+++ src/app/lonely/lonely.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { LonelyService } from '../shared/services/lonely.service';
 import { Settings } from '../shared/models/settings';
 
+import * as moment from 'moment';
+
 @Component({
     selector: 'app-lonely',
     templateUrl: './lonely.component.html',
@@ -26,8 +28,12 @@ export class LonelyComponent {
 
     setLonely(): void {
         this.form.patchValue({
-            lonelyDateTime: new Date()
+            lonelyDateTime: moment()
         });
+    }
+
+    isLonely(): boolean {
+        return moment().diff(this.form.value.lonelyDateTime, 'days') < 1;
     }
 
     saveSettings(): void {
